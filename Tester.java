68d5da2746cf6829eeb985fb88e1ec302bbbd1c4
GowthamS8485 <108718022+GowthamS8485@users.noreply.github.com>
2022-07-19 10:11:22 +0530

    Method code

---
+++ Tester.java
@@ -0,0 +1,21 @@
+package com.schoolrecords.chennai;
+
+import java.util.Scanner;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SchoolRecords k = new SchoolRecords();
+		String SchoolName;
+		int SchoolId;
+		String SchoolAddress;
+		Scanner sc = new Scanner(System.in);
+		SchoolId = sc.nextInt();
+		SchoolName = sc.next();
+		SchoolAddress = sc.next();
+		k.ddRecords();
+		k.dispRecords(SchoolId,SchoolName,SchoolAddress);
+	}
+
+}
