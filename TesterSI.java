d897d89fed56a875569607fd1b1d83d0180eadff
GowthamS8485 <108718022+GowthamS8485@users.noreply.github.com>
2022-07-19 10:15:01 +0530

    method code

---
+++ TesterSI.java
@@ -0,0 +1,12 @@
+package com.SIpackage;
+
+public class TesterSI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SI k = new SI();
+		k.SimpleInterest(10000,24,2);
+
+	}
+
+}
